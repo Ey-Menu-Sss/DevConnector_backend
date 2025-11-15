a79a7b475ba5ec7e84905cab662b36ddfaf5e3e5
Muhammad Qodir <muhammadeymenus@gmail.com>
2025-11-16 01:14:00 +0500

    ws added

--- DevConnector_back/settings.py
+++ DevConnector_back/settings.py
@@ -168,12 +168,7 @@ CHANNEL_LAYERS = {
         # "BACKEND": "channels.layers.InMemoryChannelLayer" 
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [
-                {
-                    "address": os.getenv("REDIS_URL"),
-                    "ssl": True,
-                }
-            ],
+            "hosts": [os.getenv("REDIS_URL")],
         },
     }
 }
